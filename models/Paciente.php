31fbe928a824bfadf7413796fb91a4fb08a77f95
marino5710 <brianmarin4@gmail.com>
2023-08-05 17:58:21 -0600

    se termino de crear paciente modelo

--- models/Paciente.php
+++ models/Paciente.php
@@ -6,3 +6,21 @@ class Especialidad extends ActiveRecord{
     public static $tabla = 'pacientes';
     public static $columnasDB = ['paciente_nombre','paciente_dpi','paciente_telefono', 'paciente_situacion'];
     public static $idTabla = 'paciente_id';
+
+
+    public $paciente_id;
+    public $paciente_nombre;
+    public $paciente_dpi;
+    public $paciente_telefono;
+    public $paciente_situacion;
+
+
+    public function __construct($args = [] )
+    {
+        $this->paciente_id = $args['paciente_id'] ?? null;
+        $this->paciente_nombre = $args['paciente_nombre'] ?? '';
+        $this->paciente_dpi = $args['paciente_dpi'] ?? '';
+        $this->paciente_telefono = $args['paciente_telefono'] ?? '';
+        $this->paciente_situacion = $args['paciente_situacion'] ?? '1';
+    }
+}
