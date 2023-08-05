4483e53b6b67e28b89a4cc0588ce2b2d459d7572
marino5710 <brianmarin4@gmail.com>
2023-08-05 14:25:42 -0600

    se creo el archivo index js de especialidades

--- public/index.php
+++ public/index.php
@@ -8,6 +8,12 @@ $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [AppController::class,'index']);
+$router->get('/especialides', [EspecialidadController::class,'index'] );
+$router->post('/API/especialides/guardar', [EspecialidadController::class,'guardarAPI'] );
+$router->post('/API/especialidades/modificar', [EspecialidadController::class,'modificarAPI'] );
+$router->post('/API/especialidades/eliminar', [EspecialidadController::class,'eliminarAPI'] );
+$router->get('/API/especialidades/buscar', [EspecialidadController::class,'buscarAPI'] );
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
