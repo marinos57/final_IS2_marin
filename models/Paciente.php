0aebaaa323ef247455db74b15bd0c1e7f0956005
marino5710 <brianmarin4@gmail.com>
2023-08-05 18:14:12 -0600

    se modifico el modelo y el controller

--- models/Paciente.php
+++ models/Paciente.php
@@ -2,7 +2,7 @@
 
 namespace Model;
 
-class Especialidad extends ActiveRecord{
+class Paciente extends ActiveRecord{
     public static $tabla = 'pacientes';
     public static $columnasDB = ['paciente_nombre','paciente_dpi','paciente_telefono', 'paciente_situacion'];
     public static $idTabla = 'paciente_id';
