ac9678f746c5b574ca048a417170cd3ed31b5a07
marino5710 <brianmarin4@gmail.com>
2023-08-06 20:45:33 -0600

    se comentario el const headers

--- src/js/medicos/index.js
+++ src/js/medicos/index.js
@@ -75,12 +75,12 @@ const buscar = async () => {
     let medico_especialidad = formulario.medico_especialidad.value;
     let medico_clinica = formulario.medico_clinica.value;
     const url = `/final_IS2_marin/API/medicos/buscar?medico_nombre=${medico_nombre}&medico_especialidad=${medico_especialidad}&medico_clinica=${medico_clinica}`;
-    const headers = new Headers();
-    headers.append("X-Requested-With", "fetch");
+    // const headers = new Headers();
+    // headers.append("X-Requested-With", "fetch");
     const config = {
         method : 'GET',
-        headers,
-            body
+        // headers,
+            // body
     }
 
     try {
